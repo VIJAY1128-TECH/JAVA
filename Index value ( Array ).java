a5498292729559587ab466b594dbbac09cf850be
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 22:35:06 +0530

    finding the number using index value in the array

---
+++ Index value ( Array ).java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] value = {92,35,51,14,28,8,20};
+	    for(int i=0; i<value.length; i++){
+	        System.out.println(value[4]);
+	    }
+	
+}
+}
