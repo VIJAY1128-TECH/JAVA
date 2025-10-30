517d7a8b6452ab4cf6e1e8d41254417c06ecc38e
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 22:26:34 +0530

    find the year is leap year or not a leap year

---
+++ leap year or not.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int year = 2024;
+	    if ( year%4 ==0){
+	        System.out.println("Leap year");
+	    }
+	    else{
+	        System.out.println("Not a leap year");
+	    }
+	    
+	}
+}
