6ed360f1ea702f455d1e1716e794e0a0eacc452b
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 22:19:07 +0530

    find the number is odd or even in looping concept

---
+++ Looping ( number is odd or even).java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    for (int a=0; a<=100; a++)
+	
+	if ( a%2==0 ){
+	    System.out.println(a + "= Even");
+	}
+	else {
+	    System.out.println(a + "= Odd");
+	}
+	}
+}
