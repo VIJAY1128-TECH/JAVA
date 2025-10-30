b88782ef9b25df5340269deb154fac75bba91e24
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 21:34:51 +0530

    printing array

---
+++ printing array numbers.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] num ={2,3,4,5,6,7,8,9,10};
+	    for(int i=0; i<num.length; i++){
+	        System.out.println(num[i]);
+	    }
+	
+}
+}
