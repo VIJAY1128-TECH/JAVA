0d7b43eb0871322f89a3df5fdbbec3386be4a069
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 22:40:13 +0530

    Finding the given number is positive or not

---
+++ find a number is positive or negative.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args){
+	    int num = 5;
+	    if (num>=0){
+	        System.out.println(" number is positive");
+	    }
+	    else{
+	        System.out.println(" number is negative");
+	    }
+	}
+}
+	
