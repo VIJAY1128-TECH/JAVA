676e9753f2e8e95dbc9f47865596126a828eeea0
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-30 22:36:54 +0530

    show the grade of a person based on marks

---
+++ Grade based on marks.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int mark = 35;
+	    if(mark>=90){
+	        System.out.println("Grade A");
+	    }
+	    else if(mark>=75){
+	        System.out.println("Grade B");
+	    }
+	    else if(mark>=50){
+	        System.out.println("Grade C");
+	    }
+	    else{
+	        System.out.println("Fail");
+	    }
+	    }
+	}
