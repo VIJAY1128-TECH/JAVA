e41206de09aa2f8738a3522529b1b18926c0e651
VIJAY1128-TECH <vijaynathv57@gmail.com>
2025-10-31 18:37:15 +0530

    Find the factorial of a given number

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args){
+	    int n = 10;
+	    int fact = 1;
+	    for (int a=1; a<=n; a++){
+	        fact = fact*a;
+	        System.out.println(fact);
+	    }
+	    
+}
+}
